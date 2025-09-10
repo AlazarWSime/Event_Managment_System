d61804870904dfdbd1effe47362b93e9e039d53b
Alazar W. Sime <alazarwondwessen47@gmail.com>
2025-09-10 20:44:00 +0300

    Add project root to Python path in wsgi.py

--- Event_Managment_System/Event_Managment_System/wsgi.py
+++ Event_Managment_System/Event_Managment_System/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from pathlib import Path
+
+# Add the project root directory to the Python path
+project_root = Path(__file__).resolve().parent.parent
+sys.path.append(str(project_root))
 
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Event_Managment_System.Event_Managment_System.settings')
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Event_Managment_System.Event_Managment_System.settings')
 
 application = get_wsgi_application()
